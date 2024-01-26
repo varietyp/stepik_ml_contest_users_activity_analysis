763eb9a026f6f8e4d28ce4198715eaa801a15306
varya_pp <varyamint@gmail.com>
2024-01-26 17:48:17 +0300

    add finding the hardest step for most users

--- data_analysis/final_step_for_most_users.py
+++ data_analysis/final_step_for_most_users.py
@@ -1,6 +1,6 @@
 from users_actions_data import submissions_data
 
-# finding final or hardest step for most users
+# finding final or the hardest step for most users
 
 submissions = submissions_data.drop(['date', 'day'], axis=1)
 worst_step = submissions[submissions.submission_status == 'wrong']\
